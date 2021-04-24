59afe5fa47037613cb4988eff97a2abeadc30281
Barvent <67266826+Barvent@users.noreply.github.com>
2021-04-24 13:05:38 +0200

    Added OAuth2 security

--- genomic-content-service/src/main/java/mpegg/gcs/genomiccontentservice/Controller/FileController.java
+++ genomic-content-service/src/main/java/mpegg/gcs/genomiccontentservice/Controller/FileController.java
@@ -1,5 +1,7 @@
 package mpegg.gcs.genomiccontentservice.Controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +15,8 @@ public class FileController {
         return "ok";
     }
 
-    @GetMapping("/")
-    public String get() {
+    @GetMapping("/get")
+    public String get(@AuthenticationPrincipal Jwt jwt) {
         return "ok";
     }
 }
